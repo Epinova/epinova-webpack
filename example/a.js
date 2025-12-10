f717b545ad1d62bfb96fe5ef38ce81719ad0c3d5
Alexander Nilsson <alexander.nilsson@epinova.no>
2025-12-10 12:46:08 +0100

    Fix paths in webpack configuration and update build command in CI workflow

--- example/a.js
+++ example/a.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 
-import DemoComponent from './features/DemoComponent/DemoComponent.vue';
-import { store } from './store';
+import DemoComponent from './features/Vue/DemoComponent.vue';
+import { store } from './features/Vue/vuex-store';
 
 new Vue({
     components: {
